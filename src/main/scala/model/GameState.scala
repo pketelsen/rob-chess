c3a73b6294cf37c3e6f2c9fd8d9c907eeed69df2
Matthias Schiffer <mschiffer@universe-factory.net>
2016-01-21 14:11:46 +0100

    Implement model

--- src/main/scala/model/GameState.scala
+++ src/main/scala/model/GameState.scala
@@ -1,4 +1,21 @@
 package model
 
-class GameState
-class Move
+sealed class Piece
+
+case object Pawn extends Piece
+case object Rook extends Piece
+case object Knight extends Piece
+case object Bishop extends Piece
+case object King extends Piece
+case object Queen extends Piece
+
+
+sealed class Move
+
+case class BoardPos(file: Int, rank: Int)
+
+case class NormalMove(src: BoardPos, dest: BoardPos)
+case class PromotionMove(src: BoardPos, dest: BoardPos, promotion: Piece)
+
+
+case class GameState(moves: Seq[Move])
